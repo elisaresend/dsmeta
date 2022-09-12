b4338de5f1bab7095ad73cbcc7d3e134dd210e62
Elisabete Resende <182elisa182@gmail.com>
2022-09-11 21:22:27 -0300

    Toast

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import NotificationButton from "./components/NotificationButton"
-import Header from "./components/Header"
-import SalesCard from "./components/SalesCard"
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
 
 
 function App() {
   return (
     <>
+      <ToastContainer />
       <Header />
       <main>
         <section id="sales">
@@ -18,4 +20,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
